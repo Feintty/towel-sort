f901dda940c6f940e32240a393011913a77e13ec
Feintty <makhmutoff.ildar2011@yandex.ru>
2020-10-05 08:51:03 +0400

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 
 // You should implement your task here.
-
+//Solution
 module.exports = function towelSort (matrix) {
-   return this.flat(1);
+	if(matrix!==undefined){
+		for(i=0;i<matrix.length;i++)
+		{
+			matrix[i]=(i%2!==0) ? matrix[i].reverse():matrix[i];
+		}
+		return matrix.flat(1);
+	}
+	else {
+		return [];
+	}
 }
